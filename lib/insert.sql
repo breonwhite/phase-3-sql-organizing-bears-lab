92a1f6e6b7ade14a8deb7606d913a1e39e4fe953
Breon White <bdwhite92@gmail.com>
2022-09-02 13:51:20 -0400

    commit tree d1affc3c75317db9d2b51d1ddfeb762706f1af40

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'X', 'Red', 'Dopey', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'X', 'Yellow', 'Bashful', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'X', 'Green', 'Grumpy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'X', 'Blue', 'Sleepy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'X', 'Purple', 'Sneezy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 6, 'X', 'Brown', 'Happy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'X', 'Black', 'Doc', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( NULL, 8, 'X', 'White', 'Nice', TRUE);
